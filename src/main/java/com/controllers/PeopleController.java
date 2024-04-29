ca356fc01601085a087a0dbc0b015fce494d7bce
Evgeny Bayandin <5087081@gmail.com>
2024-04-29 15:58:30 +0300

    Update PeopleController

--- src/main/java/com/controllers/PeopleController.java
+++ src/main/java/com/controllers/PeopleController.java
@@ -2,11 +2,23 @@ package com.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    @GetMapping()
+    public String index(Model model){
+        return null;
+}
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        return null;
+    }
 
 
 }
